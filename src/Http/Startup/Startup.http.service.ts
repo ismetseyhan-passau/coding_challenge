9a64c4185cd8c484f0e668a42217bf646dba81eb
ismetseyhan <ismet@capo.build>
2023-09-06 17:12:18 +0200

    getAllStartups api has been implemented

--- src/Http/Startup/Startup.http.service.ts
+++ src/Http/Startup/Startup.http.service.ts
@@ -1,10 +1,20 @@
 import axios from "axios";
-import { Startup, StartupDTO } from "../../Types/Startup";
+import {Startup, StartupDTO} from "../../Types/Startup";
 import StartupMapper from "./Startup.mapper";
 
 export class StartupHttpService {
-  public static async getStartupById(id: string | number): Promise<Startup> {
-    const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
-    return StartupMapper.map(response.data);
-  }
+    public static async getStartupById(id: string | number): Promise<Startup> {
+        const response = await axios.get<StartupDTO>(`/api/startups/${id}`);
+        return StartupMapper.map(response.data);
+    }
+
+    public static async getAllStartups(): Promise<Startup[]> {
+        const response = await axios.get<StartupDTO[]>(`/api/startups?all=true`);
+        const startupsData = response.data;
+        console.log(startupsData);
+        return startupsData.map((startupDTO) => StartupMapper.map(startupDTO));
+
+    }
+
+
 }
